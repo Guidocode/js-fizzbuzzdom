33a7d1941cf28d23d9dc51d9593a7c11b87d1066
Guidocode <guidobuono@icloud.com>
2022-04-08 16:34:28 +0200

    Modifica ordine condizione if else

--- js/script.js
+++ js/script.js
@@ -34,26 +34,35 @@ for (let i=1; i <= limit; i++){
   const boxElement = document.createElement('div');
   // gli scrivo la classe
   boxElement.className = 'box';
-  // aggiungo il contenuto
-  boxElement.append(i);
+  // aggiungo il contenuto (in questo caso l'ho aggiunto nella condizione)
+  // boxElement.append(i);
   // stampo in console per vederlo
   console.log(boxElement);
 
-  // Se multiplo di 3 stampo fizz
-  if(!(i % mult3)){
-    boxElement.classList.add('bg-fizz');
-    boxElement.remove('i');
-    boxElement.append('fizz');
+  
+  // Se multiplo sia di 3 che di 5 stampo fizzbuzz
+  if(!(i % mult3) && !(i % mult5)){
+    boxElement.classList.add('bg-fizzbuzz');
+    boxElement.append('fizzbuzz');
+  
   // Se multiplo di 5 stampo buzz
   }else if(!(i % mult5)){
     boxElement.classList.add('bg-buzz');
     boxElement.append('buzz');
-  // Se multiplo sia di 3 che di 5 stampo fizzbuzz
-  }else if(!(i % mult3) && !(i % mult5)){
-    boxElement.classList.add('bg-fizzbuzz');
-    boxElement.append('fizzbuzz');
-  };
+  // Se multiplo di 3 stampo fizz
+  }else if(!(i % mult3)){
+    boxElement.classList.add('bg-fizz');
+    boxElement.append('fizz');
+  // Altrimenti stampo il numero
+  }else{
+    boxElement.append(i);
+  }
 
   // lo aggiungo al contenitore
   container.append(boxElement);
 };
+
+/* 
+  Reminder: L'ORDINE DI SCRITTURA DEL CODICE FA LA DIFFERENZA
+  Scrivere le condizioni "dall'insieme" più grande al più piccolo
+*/
